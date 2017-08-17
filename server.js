d2662cd9dd1425d310fe941e4ab0383a6e1ec90b
Tarunisree <31067767+Tarunisree@users.noreply.github.com>
2017-08-17 20:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.get('/article-one',function (req,res) {
  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function (req,res) {
-  res.send('article-two is requested and send here');  
+ res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-three',function (req,res) {
-  res.send('article-three is requested and send here');  
+app.get('/article-two',function (req,res) {
+ res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
